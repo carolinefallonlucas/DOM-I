6c763e9eb94337bc522560b2b24560c06684f538
Caroline <carolinefallonlucas@gmail.com>
2020-09-28 20:26:57 -0400

    module project 1

--- js/index.js
+++ js/index.js
@@ -14,29 +14,60 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"])
+
+let nav = document.querySelectorAll("nav a");
+nav[0].textContent = "Services"
+nav[1].textContent = "Product"
+
+
+nav.prepend("something")
+nav.append("else")
+const ctaHeader = document.querySelector("cta,button")
+ctaHeader.textContent = "DOM IS AWESOME"
+
+featuersHeader.textContent = "Features"
+featuersContent.textContent = "Features...."
+
+const ctaButton = document.querySelector("cta, button")
+ctaButton.textContent = "Get Started"
+let ctaImg = document.getElementById("cta-img")
+
+
+const featureText = document.querySelector(".text-content:nth-of-type(1)")
+featureText.textContent = siteContent["main-content"]["features-h4"]
+const aboutText = document.querySelector(".text-content:nth-of-type(2)")
+aboutText.textContent = siteContent["main-content"]["about-h4"]
+const featuresPara = document.querySelector(".text-content:nth-of-type(1)");
+featuresPara.textContent = siteContent["main-content"]["features-content"];
+const aboutPara = document.querySelector(".text-content:nth-of-type(2)");
+aboutPara.textContent = siteContent["main-content"]["about-content"];
+
+
+document.querySelector("footer").querySelector("p").innerText =
+  siteContent.footer.copyright; 
